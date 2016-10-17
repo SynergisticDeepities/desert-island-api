b1ed6000bb71b5301ac35ba8742a36f9330249b5
nishkixs <nishon.radhakrishnan@gmail.com>
2016-10-17 10:41:20 -0400

    Create upload resource and verify POST to /uploads
    - Defined routes for upload resource
    - Created uploads controller
    - Created upload schema and model
    - Add dependencies to package.json
    - Add script to upload file aws-s3
    - Working curl script for create

---
+++ app/models/upload.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const uploadSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: false,
+  },
+}, {
+  // toJSON: { virtuals: true },
+  timestamps: true,
+});
+
+uploadSchema.virtual('length').get(function length () {
+  return this.text.length;
+});
+
+const Upload = mongoose.model('Upload', uploadSchema);
+
+module.exports = Upload;
